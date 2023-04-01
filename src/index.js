6146c9fbb9ac661fec257b85541c31b17d90553f
posternaka <nik.petrenko97@gmail.com>
2023-04-01 18:35:31 +0300

    code improvement and review

--- src/index.js
+++ src/index.js
@@ -3,150 +3,158 @@ import axios from 'axios';
 
 const URL = 'https://jsonplaceholder.typicode.com/posts';
 
-const labelMap = {
-    firstname: document.querySelector('#label_firstname'),
-    lastname: document.querySelector('#label_lastname'),
-    date: document.querySelector('#label_date'),
-    email: document.querySelector('#label_email'),
-    password: document.querySelector('#label_password'),
-    checkPassword: document.querySelector('#label_checkPassword'),
-}
-
-const inputMap = {
-    firstname: document.querySelector('#firstname'),
-    lastname: document.querySelector('#lastname'),
-    date: document.querySelector('#date'),
-    email: document.querySelector('#email'),
-    password: document.querySelector('#password'),
-    checkPassword: document.querySelector('#checkPassword'),
-}
-
-const postData = async () => {
-    try {
-        const data = await axios.post(URL, {
-            name: 'Nik',
-            fam: 'Petr'
-        });
-        console.log(data.data);
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-
-const handleError = (name, error) => {
-    labelMap[name].classList.add('error');
-    inputMap[name].classList.add('input__error');
-    inputMap[name].classList.remove('input__success');
-
-    labelMap[name].innerHTML = error;
-}
-
-const handleSuccess = (name) => {
-    inputMap[name].classList.add('input__success');
-    labelMap[name].classList.remove('error');
-}
-
-
 const btn = document.querySelector('#submit');
 
-const handleFullname = (value, name) => {
+const handlerError = (name, error) => {
+    settings[name].input.classList.add('input__error');
+    settings[name].input.classList.remove('input__success');
+    settings[name].label.classList.add('label__error');
+    settings[name].label.innerHTML = error;
+}
+
+const handlerSuccess = (name) => {
+    settings[name].input.classList.add('input__success');
+    settings[name].label.classList.remove('label__error');
+}
+
+const handlerFullname = (value, name) => {
     const errorMinLength = 'You need to enter more than 1 symbol.';
     const errorMaxLength = 'You do not need to enter more than 25 symbol.';
 
     if (value.length < 2) {
-        handleError(name, errorMinLength);
-    } else if (value.length > 26) {
-        handleError(name, errorMaxLength);
+        handlerError(name, errorMinLength);
+    } else if (value.length > 25) {
+        handlerError(name, errorMaxLength);
     } else {
-        handleSuccess(name);
+        handlerSuccess(name);
     }
-
-    checkDisable()
 }
 
-const handleDate = (value, name) => {
+const handlerDate = (value, name) => {
     const errorMessage =  'Incorrect information.';
 
     if (value > new Date().toISOString().slice(0, 10)) {
-        handleError(name, errorMessage);
+        handlerError(name, errorMessage);
     } else {
-        handleSuccess(name);
+        handlerSuccess(name);
     }
-
-    checkDisable()
 }
 
-const handleEmail = (value, name) => {
+const handlerEmail = (value, name) => {
     const errorMessage = 'You forgot something)';
 
     if (!(/(.+)@(.+){2,}\.(.+){2,}/.test(value))) {
-        handleError(name, errorMessage);
+        handlerError(name, errorMessage);
     } else {
-        handleSuccess(name);
+        handlerSuccess(name);
     }
-
-    checkDisable()
 }
 
-let currentPassword = '';
 let checkPassword = '';
+let currentPassword = '';
 
-const handlePassword = (value, name) => {
+const handlerPassword = (value, name) => {
     const errorMinLength = 'You need to enter more than 8 symbol.';
     const errorUpper = 'You must have at least 1 uppercase symbol.';
     const errorNumber = 'You must have at least 1 number.';
     const errorSymbol = 'You must have at least 1 special symbol.';
 
     if (value.length < 8) {
-        handleError(name, errorMinLength);
+        handlerError(name, errorMinLength);
     } else if (!(/[A-Z]/.test(value))) {
-        handleError(name, errorUpper);
+        handlerError(name, errorUpper);
     } else if(!(/\d/.test(value))) {
-        handleError(name, errorNumber);
+        handlerError(name, errorNumber);
     } else if (!(/[`!@#$%]/.test(value))) {
-        handleError(name, errorSymbol);
+        handlerError(name, errorSymbol);
     } else {
-        handleSuccess(name);
+        handlerSuccess(name);
     }
 
     currentPassword = value;
-    checkDisable()
+    handlerCheckPassword(checkPassword, 'checkPassword');
 }
 
-const handleCheckPassword = (value, name) => {
+const handlerCheckPassword = (value, name) => {
     const errorMessage = 'Passwords do not match.'
 
     if (currentPassword !== value) {
-        handleError(name, errorMessage);
+        handlerError(name, errorMessage);
     } else {
-        handleSuccess(name);
+        handlerSuccess(name);
     }
 
     checkPassword = value;
-    checkDisable()
 }
 
+const handlerMap = {
+    firstname: handlerFullname,
+    lastname: handlerFullname,
+    date: handlerDate,
+    email: handlerEmail,
+    password: handlerPassword,
+    checkPassword: handlerCheckPassword,  
+}
 
-const checkDisable = () => {
-    const result = Object.values(inputMap).every((it) => it.className.includes('success'));
-    if (result) {
-        btn.classList.remove('disable');
-    } else {
-        btn.classList.add('disable');
+const settings = [...document.querySelectorAll('input')].reduce((acc, it) => {
+    acc[it.id] = {
+        label: document.querySelector(`#error_${it.id}`),
+        input: document.querySelector(`#${it.id}`),
+        handler: handlerMap[it.id],
+    }
+    return acc;
+}, {})
+
+
+const createUser = async (body) => {
+    try {
+        const data = await axios.post(URL, body);
+        console.log(data.data);
+    } catch (error) {
+        console.log(error);
     }
 }
 
-const handleSubmit = (e) => {
-    e.preventDefault();
-    postData();
+const postData = async () => {
+    const user = Object.entries(settings).reduce((acc, [key, value]) => {
+        acc[key] = value.input.value;
+        return acc;
+    }, {});
+
+    createUser(user);
 }
 
-inputMap.firstname.oninput = (e) => handleFullname(e.target.value, 'firstname');
-inputMap.lastname.oninput = (e) => handleFullname(e.target.value, 'lastname');
-inputMap.date.oninput = (e) => handleDate(e.target.value, 'date');
-inputMap.email.oninput = (e) => handleEmail(e.target.value, 'email');
-inputMap.password.oninput = (e) => handlePassword(e.target.value, 'password');
-inputMap.checkPassword.oninput = (e) => handleCheckPassword(e.target.value, 'checkPassword');
+const checkDisabled = () => {
+    const result = Object.values(settings).every((it) => it.input.className.includes('success'));
+    if (result) {
+        btn.classList.remove('disabled');
+    } else {
+        btn.classList.add('disabled');
+    }
+}
 
-btn.onclick = (e) => handleSubmit(e);
+const handlerSubmit = (e) => {
+    e.preventDefault();
+    postData();
+
+    Object.keys(settings).forEach((it) => {
+        settings[it].input.value = '';
+        settings[it].input.classList.remove('input__success');
+        settings[it].input.classList.remove('input__error');
+    });
+
+    checkDisabled();
+}
+
+const createValueInput = (name, setting) => {
+    settings[name].input.oninput = (e) => {
+        setting(e.target.value, [name]);
+        checkDisabled();
+    }
+}
+
+Object.entries(settings).forEach(([key, value]) => {
+    createValueInput(key, value.handler);
+})
+
+btn.onclick = (e) => handlerSubmit(e);
--- webpack.config.js
+++ webpack.config.js
@@ -6,50 +6,45 @@ const devServer = (isDev) => !isDev ? {} : {
     devServer: {
         open: true,
         hot: true,
-        port: 8080,
+        port: 3000,
     }
 };
 
-module.exports = ({ develop }) => ({
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js',
-        clean: true,
-    },
-    mode: develop ? 'development' : 'production',
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "codabrasoft",
-            template: './src/index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: './src/styles/main.css'
-        })
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(?:ico|png|jpg|jpeg|svg)$/i,
-                type: 'asset/inline'
-            },
-            {
-                test: /\.html$/i,
-                loader: 'html-loader'
-            },
-            {
-                test: /\.css$/i,
-                use: [
-                    MiniCssExtractPlugin.loader, 'css-loader'
-                ]
-            },
-            {
-                test: /\.scss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
-                ]
-            }
-        ]
-    },
-    ...devServer(develop),
+module.exports = ({develop}) => ({
+  mode: develop ? 'development' : 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    clean: true,
+  },
+  plugins: [
+      new HtmlWebpackPlugin({
+          template: './src/index.html'
+      }),
+      new MiniCssExtractPlugin({
+          filename: './styles/main.css'
+      })
+  ],
+  module: {
+      rules: [
+          {
+              test: /\.html$/i,
+              loader: 'html-loader'
+          },
+          {
+              test: /\.css$/i,
+              use: [
+                MiniCssExtractPlugin.loader, 'css-loader'
+              ]
+          },
+          {
+              test: /\.scss$/i,
+              use: [
+                MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
+              ]
+          }
+      ]
+  },
+  ...devServer(develop),
 });
